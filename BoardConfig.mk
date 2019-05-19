1cfa90f04048d558018008bbdc4a4d4394b5ea0d
rpeter85 <deus@blackpanther.hu>
2019-05-19 09:14:48 +0200

    Porting ShihabZzz's Update Dexpreopt commit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,9 +114,13 @@ BOARD_USES_QCNE := true
 
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
-  endif
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_DEBUG_INFO := false
+      USE_DEX2OAT_DEBUG := false
+      DONT_DEXPREOPT_PREBUILTS := true
+      WITH_DEXPREOPT_PIC := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+      PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
 endif
 
 # DRM
